d0095aeb1ba525cf8048f2b6ed94cba1eba39ab4
Wan Chang Lai <wanchang.lai@live.com>
2022-11-27 12:20:13 +0800

    Make the 'Add a Quote' Button Runable and Add some Dummy Variable

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,17 @@
-const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+import { Fragment } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Wc", text: "Reading React is fun." },
+  { id: "q2", author: "WanChang", text: "Reading React is great." },
+];
+
+const AllQuotes = (props) => {
+  return (
+    <Fragment>
+      <QuoteList quotes={DUMMY_QUOTES} />
+    </Fragment>
+  );
 };
 
 export default AllQuotes;
