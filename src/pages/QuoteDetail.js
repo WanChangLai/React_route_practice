64d166e278b0d7ff5580f90a6a1c448238c517f0
Wan Chang Lai <wanchang.lai@live.com>
2022-11-27 13:32:11 +0800

    Outputting data on a Detail Page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,26 @@
 import { useParams, Route } from "react-router-dom";
+
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Wc", text: "Reading React is fun." },
+  { id: "q2", author: "WanChang", text: "Reading React is great." },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <section>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route exact path={"/quotes/:quoteId/comments"}>
+      <HighlightedQuote author={quote.author} text={quote.text} />
+      <Route exact path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </section>
